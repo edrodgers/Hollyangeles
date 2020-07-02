a7f92009ff73e9bf7cc96d4a65ed39003e7cb1ba
edrodgers <erodgers@jakprints.com>
2020-07-02 13:58:11 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//menu hide/show
+document.querySelector(".btn").onclick = function () {
+  if (
+    this.classList.contains("not-active") ||
+    this.classList.contains("change")
+  ) {
+    this.classList.toggle("change");
+    this.classList.toggle("not-active");
+  } else {
+    this.classList.toggle("change");
+  }
+  // $(this).toggleClass("change").toggleClass("not-active");
+  document.querySelector("nav").classList.toggle("nav-open");
+};
+
+//header hide/show
+let startPos = window.pageYOffset;
+
+window.onscroll = function () {
+  console.log("working");
+  let currentPos = window.pageYOffset;
+
+  if (startPos > currentPos) {
+    document.querySelector("header").style.top = "0";
+  } else {
+    document.querySelector("header").style.top = "-100px";
+  }
+  startPos = currentPos;
+};
